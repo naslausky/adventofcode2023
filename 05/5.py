8fb74ae4b955054c2fb33146e3d6a87a61731f64
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2023-12-06 13:27:09 -0300

    Ajuste quinto dia.

--- 05/5.py
+++ 05/5.py
@@ -26,7 +26,7 @@ def percorrerConversoes(semente): # Função da parte 1 que passa uma semente po
 		conversoes = mapaConversoes[(estadoAtual, destino)]
 		for conversao in conversoes: # Para cada conversão, verificar se esta semente se encaixa nela.
 			comecoDestino, comecoOrigem, comprimento = conversao
-			if localAtual >= comecoOrigem and localAtual <= comecoOrigem + comprimento: # Poderia ser usado in range(a, b).
+			if localAtual >= comecoOrigem and localAtual < comecoOrigem + comprimento: # Poderia ser usado in range(a, b).
 				localAtual = comecoDestino + (localAtual - comecoOrigem)
 				break # Cada número só deve passar por uma conversão por etapa.
 		estadoAtual = destino # Avança uma etapa.
@@ -39,13 +39,13 @@ for semente in sementes:
 print('A menor localização possível referente às sementes iniciais é:', min(locaisSementes))
 
 # Parte 2:
-intervalosATestar = [] # Lista com os intervalos a se testar. Deve ser atualizada a cada etapa.
+intervalosATestar = set() # Lista com os intervalos a se testar. Deve ser atualizada a cada etapa.
 for indiceInformacoesSementes in range(int(len(sementes)/2)): # Reinterpreta a linha inicial para montar os intervalos de sementes.
 	indice = indiceInformacoesSementes * 2
 	inicioIntervalo = sementes[indice]
 	comprimentoIntervalo = sementes[indice + 1]
-	intervalosATestar.append((inicioIntervalo, inicioIntervalo+comprimentoIntervalo))
-	
+	intervalosATestar.add((inicioIntervalo, inicioIntervalo+comprimentoIntervalo-1))
+
 estadoAtual = 'seed'
 while estadoAtual != 'location': # Para cada etapa:
 	destino = [caminho[1] for caminho in mapaConversoes if caminho[0] == estadoAtual][0] # Obtem o nome da próxima etapa e suas conversões.
@@ -55,49 +55,47 @@ while estadoAtual != 'location': # Para cada etapa:
 	# Neste caso, quebrar os intervalos a se testar afetados:
 	for conversao in conversoes:
 		comecoDestino, comecoOrigem, comprimento = conversao
-		fimOrigem = comecoOrigem + comprimento
+		fimOrigem = comecoOrigem + comprimento - 1
 		for intervalo in intervalosATestar.copy(): # Copiar para não alterar o que se está iterando.
 			quebrouComeco = False # Variaveis que definem se houve uma quebra de intervalo para o começo ou fim de uma conversão.
 			quebrouFim = False
-
 			# O lado da conversão faz diferença na hora da quebra para saber em qual lado vai ficar o número central.
-			if comecoOrigem >= intervalo[0] and comecoOrigem <= intervalo[1]: # O começo da conversão cai dentro de um intervalo.
+			if comecoOrigem > intervalo[0] and comecoOrigem <= intervalo[1]: # O começo da conversão cai dentro de um intervalo.
 				quebrouComeco = True # Quebra o intervalo em 2:
-				intervalosATestar.append((intervalo[0], comecoOrigem - 1)) # Do começo até o começo da conversão,
-				intervalosATestar.append((comecoOrigem, intervalo[1])) # E do começo da conversão até o final.
+				intervalosATestar.add((intervalo[0], comecoOrigem - 1)) # Do começo até o começo da conversão,
+				intervalosATestar.add((comecoOrigem, intervalo[1])) # E do começo da conversão até o final.
 			# Idem para o final da conversão:
-			if fimOrigem >= intervalo[0] and fimOrigem <= intervalo[1]: # O fim da conversão cai dentro de um intervalo.
+			if fimOrigem >= intervalo[0] and fimOrigem < intervalo[1]: # O fim da conversão cai dentro de um intervalo.
 				quebrouFim = True
 				if quebrouComeco: # Neste caso, os dois lados da conversão estão contidos dentro do intervalo. Deve ser quebrado em 3.
 					intervalosATestar.remove((comecoOrigem, intervalo[1])) # Remove o intervalo criado acima, e adiciona:
-					intervalosATestar.append((comecoOrigem, fimOrigem)) # Do começo da quebra anterior até o fim da conversão,
-					intervalosATestar.append((fimOrigem + 1, intervalo[1])) # E do fim da conversão até o fim do intervalo.
+					intervalosATestar.add((comecoOrigem, fimOrigem)) # Do começo da quebra anterior até o fim da conversão,
+					intervalosATestar.add((fimOrigem + 1, intervalo[1])) # E do fim da conversão até o fim do intervalo.
 				else: # Significa que apenas o final da conversão cai dentro de um intervalo. Adicionar:
-					intervalosATestar.append((intervalo[0], fimOrigem)) # Do começo do intervalo até o final da conversão,
-					intervalosATestar.append((fimOrigem + 1, intervalo[1])) # E do final da conversão até o final do intervalo.
+					intervalosATestar.add((intervalo[0], fimOrigem)) # Do começo do intervalo até o final da conversão,
+					intervalosATestar.add((fimOrigem + 1, intervalo[1])) # E do final da conversão até o final do intervalo.
 
 			if quebrouComeco or quebrouFim: # Se houve alguma quebra, o intervalo antigo não deve ser considerado.
 				intervalosATestar.remove(intervalo)
 
-
 	# 2) Com os intervalos quebrados corretamente, tenho a certeza que cada um usa apenas uma conversão.
 	# Pra cada intervalo a ser testado, passar ele pela sua conversão e anotar a nova faixa de intervalos, que vai ser o começo da próxima etapa.
-	novosIntervalos = [] 
+	novosIntervalos = set() 
 	for comecoIntervalo, fimIntervalo in intervalosATestar: # Como cada intervalo só usa uma conversão, apenas as extremidades precisam ser convertidas.
 		novoComeco = comecoIntervalo # Novas extremidades. Devem ficar iguais caso não estejam no escopo de nenhuma conversão.
 		novoFim = fimIntervalo
 		for conversao in conversoes: 
 			comecoDestino, comecoOrigem, comprimento = conversao
-			if comecoIntervalo >= comecoOrigem and comecoIntervalo <= comecoOrigem + comprimento: # Converte o começo do intervalo.
+			if comecoIntervalo >= comecoOrigem and comecoIntervalo < comecoOrigem + comprimento: # Converte o começo do intervalo.
 				novoComeco = comecoDestino + (comecoIntervalo - comecoOrigem)
 				break
 
 		for conversao in conversoes: # Poderia ser feito junto com o for acima, mas precisaria de um controle para não converter o mesmo lado múltiplas vezes. 
 			comecoDestino, comecoOrigem, comprimento = conversao
-			if fimIntervalo >= comecoOrigem and fimIntervalo <= comecoOrigem + comprimento: # Converte o fim do intervalo.
+			if fimIntervalo >= comecoOrigem and fimIntervalo < comecoOrigem + comprimento: # Converte o fim do intervalo.
 				novoFim = comecoDestino + (fimIntervalo - comecoOrigem)
 				break
-		novosIntervalos.append((novoComeco, novoFim))
+		novosIntervalos.add((novoComeco, novoFim))
 	intervalosATestar = novosIntervalos # Sobrescreve os intervalos a testar para a próxima etapa.
 	estadoAtual = destino # Atualiza a etapa.
 
