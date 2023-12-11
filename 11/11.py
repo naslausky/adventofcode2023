dd9e8179250f099649e86d96a7dd37b4da289669
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2023-12-11 10:12:50 -0300

    Ajusta resolução do décimo primeiro dia.

--- 11/11.py
+++ 11/11.py
@@ -11,29 +11,41 @@ for indiceLinha, linha in enumerate(linhas):
 		if caracter == '#':
 			coordenadasEstrelas.append((indiceLinha, indiceCaracter))
 
-colunasEstrelas = [y for _, y in coordenadasEstrelas]
+colunasEstrelas = list({y for _, y in coordenadasEstrelas}) # Lista que contém as colunas que contém estrelas, ordenada e sem duplicatas.
 colunasEstrelas.sort()
-linhasEstrelas = [x for x, _ in coordenadasEstrelas]
+indicesColunasEstrelas = {coluna: indice for indice, coluna in enumerate(colunasEstrelas)}
+
+linhasEstrelas = list({x for x, _ in coordenadasEstrelas}) # Idem para as linhas.
 linhasEstrelas.sort()
+indicesLinhasEstrelas = {linha: indice for indice, linha in enumerate(linhasEstrelas)}
 
 resposta = 0
 respostaParte2 = 0
 for indiceEstrela1, coordenadaEstrela1 in enumerate(coordenadasEstrelas):
-	for coordenadaEstrela2 in coordenadasEstrelas[indiceEstrela1 + 1:]:
+	for coordenadaEstrela2 in coordenadasEstrelas[indiceEstrela1 + 1:]: # Calcula apenas para as estrelas seguintes.
 		x1, y1 = coordenadaEstrela1
 		x2, y2 = coordenadaEstrela2
-		x1, x2 = sorted((x1, x2))
+		x1, x2 = sorted((x1, x2)) # Ordena as duas linhas e colunas utilizadas.
 		y1, y2 = sorted((y1, y2))
-		distanciaBase = x2 - x1 + y2 - y1
+		distanciaBase = (x2 - x1) + (y2 - y1)
 		
-		quantidadeLinhasDuplicadas = len([1 for x in range(x1, x2) if x not in linhasEstrelas])
-		quantidadeColunasDuplicadas = len([1 for y in range(y1, y2) if y not in colunasEstrelas])
-		quantidadeDuplicadas = quantidadeLinhasDuplicadas + quantidadeColunasDuplicadas
+		quantidadeLinhasDuplicadas = 0
+		indiceX1 = indicesLinhasEstrelas[x1] # Índices para percorrer as listas de linhas/colunas.
+		indiceX2 = indicesLinhasEstrelas[x2] # Para cada elemento, a distância para o elemento anterior é a quantidade de linhas/colunas vazias.
+		for linha1, linha2 in zip(linhasEstrelas[indiceX1: indiceX2], linhasEstrelas[indiceX1 + 1: indiceX2 + 1]):
+			quantidadeLinhasDuplicadas += linha2 - linha1 - 1
 
+		quantidadeColunasDuplicadas = 0
+		indiceY1 = indicesColunasEstrelas[y1]
+		indiceY2 = indicesColunasEstrelas[y2]
+		for coluna1, coluna2 in zip(colunasEstrelas[indiceY1: indiceY2], colunasEstrelas[indiceY1 + 1: indiceY2 + 1]):
+			quantidadeColunasDuplicadas += coluna2 - coluna1 - 1
+
+		quantidadeDuplicadas = quantidadeLinhasDuplicadas + quantidadeColunasDuplicadas
 		distanciaFinal = distanciaBase + quantidadeDuplicadas
 		distanciaFinalParte2 = distanciaBase + (1000000 - 1) * quantidadeDuplicadas
 		resposta += distanciaFinal
 		respostaParte2 += distanciaFinalParte2
 
-print(resposta)
-print(respostaParte2)
+print('A soma da quantidade mínima de passos entre cada par de estrelas é:', resposta)
+print('A soma da quantidade mínima de passos entre cada par de estrelas com o universo mais expandido é:', respostaParte2)
