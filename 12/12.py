57fbe624f0016bac8f77aa02895cb2fcee01ebc2
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2023-12-13 13:15:01 -0300

    Ajusta a resolução do décimo segundo dia.

--- 12/12.py
+++ 12/12.py
@@ -1,55 +1,49 @@
+# Desafio do dia 12/12/2023:
+# a) Receber uma linha de um nonograma, e as quantidades de blocos. Calcular quantas possibilidades de preenchimento existem.
+# b) Idem porém para cada informação multiplicada por 5.
+
 with open('input.txt') as file:
 	linhas = file.read().splitlines()
 
-gabarito = {}
-def quantidadeDePossibilidades(configuracao, quantidades):
+gabarito = {} # Caso a função já tenha sido chamada com as mesmas entradas, não precisar calcular novamente.
+def quantidadeDePossibilidades(configuracao, quantidades): 
+	# Função que recebe como entrada uma string, e a tupla com os blocos a serem analisados,
+	# e retorna a quantidade de organizações possíveis.
+
 	chave = (configuracao, quantidades)
-	if chave in gabarito:
+	if chave in gabarito: # Verifica se já foi calculado anteriormente.
 		return gabarito[chave]
-	ultimoElemento = len(quantidades) == 1
-	tamanhoMinimo = sum(quantidades) + len(quantidades) - 1
-	maiorIndiceAPercorrer = len(configuracao) - tamanhoMinimo + 1
+
+	ultimoElemento = len(quantidades) == 1 # Booleano que informa se estamos no elemento final.
+	tamanhoMinimo = sum(quantidades) + len(quantidades) - 1 # O mínimo de caracteres necessários para acomodar todos os blocos.
+	if len(configuracao) < tamanhoMinimo:
+		return 0
+
+	maiorIndiceAPercorrer = len(configuracao) - tamanhoMinimo + 1 # Ao percorrer as possibilidades para o início do primeiro bloco, só podemos ir até onde ainda sobra espaço suficiente para os próximos.
 	
 	indicePrimeiroDanificado = configuracao.find('#')
-	if indicePrimeiroDanificado != -1:
+	if indicePrimeiroDanificado != -1: # Se o primeiro bloco começar após um '#', ele não seria o primeiro bloco e portanto este também é um limite superior de busca.
 		maiorIndiceAPercorrer = min(maiorIndiceAPercorrer, indicePrimeiroDanificado + 1)
-#		print(configuracao, quantidades, indicePrimeiroDanificado, len(configuracao)-tamanhoMinimo )
-#		input()
-	if len(configuracao) < tamanhoMinimo:
-		#print('Tamanho insuficiente')
-		return 0
+
 	blocoDaVez = quantidades[0]
 	resposta = 0
-	for indice in range(maiorIndiceAPercorrer):
-		#print('\t Começando pelo indice:', indice, 'da string', configuracao)
-		candidato = configuracao[indice : indice + blocoDaVez]
-		if '.' in candidato:
-			#print('\t Indice não encaixa:', candidato)
+	for indice in range(maiorIndiceAPercorrer): # Indice para início do primeiro bloco.
+		candidato = configuracao[indice : indice + blocoDaVez] # Janela para verificação do bloco em questão.
+		if '.' in candidato: # A janela experimentada não serve.
 			continue
-		if ultimoElemento: # É uma possibilidade e só falta ele. Adicionar uma possibilidade à resposta.
-#			print('\t é o último elemento')
-#			print(configuracao)
-#			print(candidato)
-			if indice+blocoDaVez < len(configuracao) and '#' in configuracao[indice+blocoDaVez:]:
+
+		if ultimoElemento: # Caso ele seja o último, este índice é uma possibilidade apenas se não existir outro '#' no resto da string (pois neste caso ele não seria verdadeiramente o último).
+			if not (indice+blocoDaVez < len(configuracao) and '#' in configuracao[indice+blocoDaVez:]):
+				resposta += 1
+		else: # Caso esse não seja o último bloco, é necessario ter o separador.
+			if configuracao[indice + blocoDaVez] == '#': # O separador pode ser '?' ou '.'.
 				continue
-			resposta += 1
-		else: 
-			if configuracao[indice + blocoDaVez] == '#': # Se não é o último, tem que ter o separador. Pode ser ? ou .
-				#print('\t Não tem o . após pra ter espaco.')
-				continue
-			if indice-1 > 0 and configuracao[indice - 1] == '#':
-				#significa que não tem o espaço anterior.
-				#print('\t não tem o . antes pra ter espaço.')
-				continue
-			#print('\t Chamando com', configuracao[indice+blocoDaVez+1:], quantidades[1:])
+			# Para cada candidato não sendo o último elemento, chama a função recursiva com
+			# um bloco a menos e o resto da string que sobrou após retirar o candidato e o separador.
 			resposta += quantidadeDePossibilidades(configuracao[indice+blocoDaVez+1:], quantidades[1:])
-			#print('\t Voltou da chamada')
-	gabarito[chave] = resposta
+	gabarito[chave] = resposta # Salva para não precisar calcular no futuro.
 	return resposta
 
-#print(quantidadeDePossibilidades('?###????????', (3,2,1)))
-
-#exit()
 resposta = 0
 respostaParte2 = 0
 for linha in linhas:
@@ -61,7 +55,5 @@ for linha in linhas:
 	configuracao = '?'.join(configuracao)
 	quantidades = 5 * quantidades
 	respostaParte2 += quantidadeDePossibilidades(configuracao, quantidades)
-	#print(linha, resposta)
-	#input()
-print(resposta)
-print(respostaParte2)
+print('A soma da quantidade de arranjos diferentes é:', resposta)
+print('A soma da quantidade de arranjos diferentes com 5x cada linha é:', respostaParte2)
