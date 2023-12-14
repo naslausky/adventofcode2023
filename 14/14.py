76ba512a050104b176fb088df030a9fe900563d6
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2023-12-14 11:43:10 -0300

    Ajusta a solução do décimo quarto dia.

--- 14/14.py
+++ 14/14.py
@@ -1,8 +1,15 @@
+# Desafio do dia 14/12/2023:
+# a) Receber um mapa com esferas e paredes. Calcular o estado final após inclinar as esferas para rolarem para norte.
+# b) Calcular o estado final após realizar 1000000000 ciclos de inclinações para os quatro lados.
+
 with open('input.txt') as file:
 	linhas = file.read().splitlines()
 
-paredes = set()
-esferas = set()
+paredes = set() # Conjunto das coordenadas das paredes que as esferas não podem atravessar.
+esferas = set() # Conjunto das coordenadas das esferas.
+numeroDeLinhas = len(linhas)
+numeroDeColunas = len(linhas[0])
+
 for indiceLinha, linha in enumerate(linhas):
 	for indiceCaracter, caracter in enumerate(linha):
 		coordenada = (indiceLinha, indiceCaracter)
@@ -11,7 +18,7 @@ for indiceLinha, linha in enumerate(linhas):
 		elif caracter == 'O':
 			esferas.add(coordenada)
 
-def imprimirMapa():
+def imprimirMapa(): # Função de depuração que imprime o estado atual das esferas.
 	for indiceLinha in range(numeroDeLinhas):
 		for indiceCaracter in range(numeroDeColunas):
 			chave = (indiceLinha, indiceCaracter)
@@ -23,71 +30,55 @@ def imprimirMapa():
 			print(caracter, end = '')
 		print('')
 
-numeroDeLinhas = len(linhas)
-numeroDeColunas = len(linhas[0])
+def moverEsferasEmUmaDirecao(esferas, direcao): # Função que recebe um conjunto de esferas e move todas ao máximo para a direção dada.
+	algumaEsferaMoveu = True
+	while algumaEsferaMoveu:
+		algumaEsferaMoveu = False
+		coordenadasARemover = set()
+		coordenadasAAdicionar = set()
+		for esfera in esferas:
+			proximaCoordenada = (esfera[0] + direcao[0], esfera[1] + direcao[1])
+			if (proximaCoordenada[0] not in range(numeroDeLinhas) 
+				or proximaCoordenada[1] not in range(numeroDeColunas)):
+				continue
+			if proximaCoordenada not in esferas and proximaCoordenada not in paredes: # Pode mover.
+				coordenadasARemover.add(esfera)
+				coordenadasAAdicionar.add(proximaCoordenada)
+				algumaEsferaMoveu = True
+		esferas-= coordenadasARemover
+		esferas.update(coordenadasAAdicionar)
+
+def calcularPeso(esferas): # Função que calcula o peso final das esferas.
+	pesoTotal = 0
+	for esfera in esferas:
+		pesoTotal+= numeroDeLinhas - esfera[0]
+	return pesoTotal
+
 norte = (-1, 0)
 sul = (1, 0)
 oeste = (0, -1)
 leste = (0, 1)
 direcoes = (norte, oeste, sul, leste) # Na ordem certa do ciclo.
 esferasParte2 = esferas.copy() # Salva para a parte 2.
-algumaEsferaMoveu = True
-while algumaEsferaMoveu:
-	algumaEsferaMoveu = False
-	coordenadasARemover = set()
-	coordenadasAAdicionar = set()
-	for esfera in esferas:
-		coordenadaNorte = (esfera[0] + norte[0], esfera[1] + norte[1])
-		if coordenadaNorte[0] < 0:
-			continue
-		if coordenadaNorte not in esferas and coordenadaNorte not in paredes: # Pode mover.
-			coordenadasARemover.add(esfera)
-			coordenadasAAdicionar.add(coordenadaNorte)
-			algumaEsferaMoveu = True
-	esferas-= coordenadasARemover
-	esferas.update(coordenadasAAdicionar)
-
-pesoTotal = 0
-for esfera in sorted(esferas):
-	pesoTotal+= numeroDeLinhas - esfera[0]
-print(pesoTotal)
+moverEsferasEmUmaDirecao(esferas, norte)
+print('O peso total nas vigas norte após uma movimentação é de:', calcularPeso(esferas))
 
 # Parte 2:
 numeroDeCiclos = 1000000000
 indiceCiclo = 0
 esferas = esferasParte2
-ciclos= {}
+ciclos = {} # Mapa para armazenar caso um estado do tabuleiro já tenha ocorrido.
 while indiceCiclo < numeroDeCiclos:
 
-	chave = frozenset(esferas)
-	if chave in ciclos: # repetiu
+	chave = frozenset(esferas) # Chave para salvar os estados.
+	if chave in ciclos: # Significa que este estado já aconteceu em um ciclo anterior.
 		periodo = indiceCiclo - ciclos[chave]
 		passosAPular = (numeroDeCiclos - indiceCiclo) // periodo
 		indiceCiclo += (passosAPular * periodo)
 	ciclos[chave] = indiceCiclo
 
 	for direcao in direcoes:
-#		print('Direcao', direcao)
-		# Dá uma rolada full nessa direção.
-		algumaEsferaMoveu = True
-		while algumaEsferaMoveu:
-			algumaEsferaMoveu = False
-			coordenadasARemover = set()
-			coordenadasAAdicionar = set()
-			for esfera in esferas:
-				proximaCoordenada = (esfera[0] + direcao[0], esfera[1] + direcao[1])
-				if (proximaCoordenada[0] not in range(numeroDeLinhas) 
-					or proximaCoordenada[1] not in range(numeroDeColunas)):
-					continue
-				if proximaCoordenada not in esferas and proximaCoordenada not in paredes: # Pode mover.
-					coordenadasARemover.add(esfera)
-					coordenadasAAdicionar.add(proximaCoordenada)
-#					print('moveu', esfera, proximaCoordenada)
-					algumaEsferaMoveu = True
-			esferas-= coordenadasARemover
-			esferas.update(coordenadasAAdicionar)
+		moverEsferasEmUmaDirecao(esferas, direcao)
 	indiceCiclo+= 1
-pesoTotal = 0
-for esfera in sorted(esferas):
-	pesoTotal+= numeroDeLinhas - esfera[0]
-print(pesoTotal)
+
+print('O peso total nas vigas norte após 1000000000 ciclos é de:', calcularPeso(esferas))
