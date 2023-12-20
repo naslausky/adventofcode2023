9dd934116d22330cb41eb5e3f9c662c053122f27
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2023-12-19 21:00:25 -0300

    Adiciona a resolução da segunda parte do décimo nono dia.

--- 19/19.py
+++ 19/19.py
@@ -7,7 +7,7 @@ with open('input.txt') as file:
 class NoVerificacao:
 	def __init__(self, regras):
 		# regras = a<2006:qkq,m>2090:A,rfg
-
+		self.destinosPossiveis = {}
 		self.verificacao, caminhos = regras.split(':', 1) # a<2006
 		self.caminhoVerdadeiro, self.caminhoFalso = caminhos.split(',', 1)
 
@@ -17,6 +17,8 @@ class NoVerificacao:
 		if ':' in self.caminhoFalso:
 			self.caminhoFalso = NoVerificacao(self.caminhoFalso)
 		# Cada caminho pode ser uma string ou um Nó para outra verificação.	
+
+		self.gerarDestinosPossiveis()
 
 	def percorrerPeca(self, peca):
 		verificacaoComValores = self.verificacao
@@ -29,11 +31,42 @@ class NoVerificacao:
 
 		return proximoNoAAvaliar.percorrerPeca(peca)
 
+	def verificacaoInversa(self,):
+		if '>' in self.verificacao:
+			nome, valor = self.verificacao.split('>')
+			valor = str(int(valor) + 1)
+			return nome + '<' + valor
+			# a > 3000 o contrario é a <= 3000, ou seja a < 3001
+		else:
+			nome, valor = self.verificacao.split('<')
+			valor = str(int(valor) - 1)
+			return nome + '>' + valor
+
+	# Eu preciso de um método que retorne uma lista de requisitos e a saída possível.
+	# {(a<3000, b>5000, c<2000) : 'bcd', (a>2999) : 'A' }
+	def gerarDestinosPossiveis(self,):
+		# 
+		if type(self.caminhoVerdadeiro) is str:
+			self.destinosPossiveis[self.verificacao] = self.caminhoVerdadeiro
+		else:
+			for chave, valor in self.caminhoVerdadeiro.destinosPossiveis.items():
+				self.destinosPossiveis[self.verificacao + ';' + chave] = valor
+
+		if type(self.caminhoFalso) is str:
+			self.destinosPossiveis[self.verificacaoInversa()] = self.caminhoFalso
+		else:
+			for chave, valor in self.caminhoFalso.destinosPossiveis.items():
+				self.destinosPossiveis[self.verificacaoInversa() + ';' + chave] = valor
+		
+
 mapaWorkflows = {}
 for workflow in workflows:
 	nome, regras = workflow.split('{')
 	regras = regras[:-1]
+#	print('Fazendo verificação:', regras)
 	verificacao = NoVerificacao(regras)
+#	print('Dicionário:', verificacao.destinosPossiveis)
+#	input()
 	mapaWorkflows[nome] = verificacao
 
 listaPecas = []
@@ -46,15 +79,76 @@ for peca in pecas:
 	listaPecas.append(dicionarioDestaPeca)
 resposta = 0
 for peca in listaPecas:
-	# peca = {a = 123, b = 345, x=789, s=12557}
-#	print('Fazendo peca:', peca)
 	workflowAtual = 'in'
 	while workflowAtual != 'R' and workflowAtual != 'A':
 		workflow = mapaWorkflows[workflowAtual]
 		workflowAtual = workflow.percorrerPeca(peca)
-#		print(workflowAtual)
-
 	if workflowAtual == 'A':
 		for valor in peca.values():
 			resposta += int(valor)
 print(resposta)
+# Parte 2:
+
+requisitosAprovacao = set()
+def percorrerDestinosAteOA(condicoesAteAgora, workflowAtual):
+	workflow = mapaWorkflows[workflowAtual]
+	for condicoes, destino in workflow.destinosPossiveis.items():
+	#{'m<1450': 'R', 'm>1449;a>2836;a>2927': 'A', 'm>1449;a>2836;a<2928': 'R', 'm>1449;a<2837': 'R'}	
+		condicoesParaEsteDestino = condicoesAteAgora + ';' + condicoes
+		if destino == 'A':
+			requisitosAprovacao.add(condicoesParaEsteDestino)
+		elif destino in mapaWorkflows:
+			percorrerDestinosAteOA(condicoesParaEsteDestino, destino)
+
+percorrerDestinosAteOA('', 'in')
+respostaParte2 = 0
+for requisito in requisitosAprovacao:
+	condicoes = [x for x in requisito.split(';') if x]
+	minimo = 'min'
+	maximo = 'max'
+	mapaDesteRequisito = {chave:{minimo:1, maximo:4000} for chave in ('x','m','a','s')}
+	for condicao in condicoes:
+		if '>' in condicao: 
+			# A> 300 significa que o minimo que A pode ser é 301
+			# se vier um A>302 depois, significa que o minimo que A pode ser é 303, ou seja é max()
+			chave = minimo
+			funcao = max
+			variavel, valor = condicao.split('>')
+			valor = int(valor) + 1
+		else:
+			chave = maximo
+			funcao = min
+			variavel, valor = condicao.split('<')
+			valor = int(valor) - 1
+
+#		if variavel not in mapaDesteRequisito:
+#			mapaDesteRequisito[variavel] = {minimo:0, maximo:5000}
+		mapaDesteRequisito[variavel][chave] = funcao(*(valor, mapaDesteRequisito[variavel][chave]))
+	#print(condicoes)
+	#['m<10', 'm>3'] # 4,5,6,7,8,9 = 6 
+	#'m': {'min': 3, 'max': 10}
+	# 
+	#print(mapaDesteRequisito)
+	produto = 1
+	for intervalo in mapaDesteRequisito.values():
+		produto *= (intervalo[maximo] - intervalo[minimo] + 1)
+	respostaParte2 += produto
+
+print(respostaParte2)
+exit()
+def maneirasParaAprovar(requisitosAteAqui, workflowAtual):
+	workflow = mapaWorkflows[workflowAtual]
+
+	print('Fazendo workflow', workflowAtual)
+	if type(workflow.caminhoVerdadeiro) is str:
+		if workflow.caminhoVerdadeiro == 'A':
+			requisitosAprovacao.add(requisitosAteAqui + workflow.verificacao)
+	else:
+		maneirasParaAprovar(requisitosAteAqui + workflow.verificacao, workflow.caminhoVerdadeiro)
+
+	if type(workflow.caminhoFalso) is str:
+		if workflow.caminhoFalso == 'A':
+			requisitosAprovacao.add(requisitosAteAqui + workflow.verificacaoInversa())
+	else:
+		maneirasParaAprovar(requisitosAteAqui + workflow.verificacaoInversa(), workflow.caminhoFalso)
+
