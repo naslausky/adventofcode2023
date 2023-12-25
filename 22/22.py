9491c81072fc88f33dd38349c402142be02f8f9e
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2023-12-25 12:00:28 -0300

    Comenta a resolução do vigésimo segundo dia.

--- 22/22.py
+++ 22/22.py
@@ -1,13 +1,15 @@
+# Desafio do dia 22/12/2023:
+# a) Receber uma lista de tijolos que estão caindo. Terminar de cair todos os tijolos e quando eles formarem uma pilha, calcular quais são seguros de remover sem que outros caiam.
+# b) Calcular quantos tijolos cairiam caso cada um deles fosse removido.
+
 import copy
 with open('input.txt') as file:
 	linhas = file.read().splitlines()
 
-
-listaPedras = [] # Lista de Conjuntos. cada conjunto é uma pedra.
-coordenadasOcupadas = set() # conjunto com todas as coordenadas ocupadas.
+listaTijolos = [] # Lista de conjuntos, em que cada conjunto são as coordenadas ocupadas por um tijolo.
+coordenadasOcupadas = set() # Conjunto com todas as coordenadas ocupadas.
 
 for linha in linhas:
-	# 6,8,103~7,8,103	
 	origem, destino = linha.split('~')
 	origem = tuple(map(int, origem.split(',')))
 	destino = tuple(map(int, destino.split(',')))
@@ -15,112 +17,68 @@ for linha in linhas:
 			 min(1, abs(origem[1] - destino[1]),),
 			 min(1, abs(origem[2] - destino[2]),))
 	setDestaPedra = {origem}
-	while origem != destino:
-		#incrementa um e adiciona no set dessa pedra
-		origem = (origem[0] + delta [0], origem[1] + delta[1], origem[2]+delta[2])
+	while origem != destino: # Adiciona todas as coordenadas intermediárias ao conjunto.
+		origem = (origem[0] + delta [0], origem[1] + delta[1], origem[2] + delta[2])
 		setDestaPedra.add(origem)
-	listaPedras.append(setDestaPedra)
+	listaTijolos.append(setDestaPedra)
 	coordenadasOcupadas.update(setDestaPedra)
 
-
-
-def estacionarPedras(listaPedras, coordenadasOcupadas):
-	# Vai, pedra a pedra, vendo se alguma pode cair:
+def estacionarTijolos(listaTijolos, coordenadasOcupadas): # Função que recebe a lista e faz todos caírem até onde dá.  
 	caiuAlguma = True
-	indicesTijolosQueCairam = set()
-	#[print(x) for x in listaPedras]
-	#print('----------')
-	#[print(x) for x in coordenadasOcupadas]
-	#input()
-	#print('listaPedras', listaPedras)
-	#print()
+	indicesTijolosQueCairam = set() # Para contabilizar para a parte 2.
 	while caiuAlguma:
 		caiuAlguma = False
-		for indiceTijolo, conjuntoPedra in enumerate(listaPedras):
-	#		print('Fazendo pedra:', conjuntoPedra)
-			#input()
-			# Para essa pedra, vê se todos os bloquinhos tem algo embaixo:
+		for indiceTijolo, tijolo in enumerate(listaTijolos): # Para um tijolo, ver se todas as coordenadas inferiores estão livres.
 			temAlgoEmbaixo = False
 			conjuntoCaidoUmaUnidade = set()
-			for coordenada in conjuntoPedra:
+			for coordenada in tijolo:
 				z = coordenada[2]
 				coordenadaInferior = (coordenada[0], coordenada[1], z-1)
-	#			print('\t coord inferior:', coordenadaInferior)
 				conjuntoCaidoUmaUnidade.add(coordenadaInferior)
 				if (coordenadaInferior in coordenadasOcupadas 
-				and coordenadaInferior not in conjuntoPedra) or (z == 1):
-	#				print('\tOpa, tem algo embaixo, não pode mais descer.')
+				and coordenadaInferior not in tijolo) or (z == 1): # Atentar-se com um tijolo que já esteja na vertical.
 					temAlgoEmbaixo = True
 					break
-			#print('Coordenada dessa após cair:', conjuntoCaidoUmaUnidade)
-			if temAlgoEmbaixo: # Não dá pra descer essa pedra, passa pra próxima
+
+			if temAlgoEmbaixo: # Não dá pra descer essa pedra, passa pra próxima.
 				continue
 			
-			# Se chegou aqui é porque dá pra cair uma:
-			indicesTijolosQueCairam.add(indiceTijolo)
+			# Se chegou aqui é porque dá pra descer este tijolo em uma unidade. Atualizar as informações:
+			indicesTijolosQueCairam.add(indiceTijolo) # Adiciona o índice do tijolo caído para a conta da parte 2.
 			caiuAlguma = True
-
-			#Atualizar o coordenadasOcupadas
-			coordenadasOcupadas-= conjuntoPedra
+			coordenadasOcupadas-= tijolo
 			coordenadasOcupadas.update(conjuntoCaidoUmaUnidade)
-
-			#Atualizar o conjuntoPedra:
-			conjuntoPedra.clear()
-			conjuntoPedra.update(conjuntoCaidoUmaUnidade)
-			#print('---------------------------------')
+			tijolo.clear()
+			tijolo.update(conjuntoCaidoUmaUnidade)
 	return len(indicesTijolosQueCairam)
 
-estacionarPedras(listaPedras, coordenadasOcupadas)
-#print('terminou')		
-#[print(x) for x in listaPedras]
-
-#print('-------------')
-#[print(x) for x in sorted(coordenadasOcupadas)]
-#print(conjuntoPedra)
-
-#print('------------')
-# Agora que todas caíram, ver para cada pedra, quais ela está em cima.
-pedrasBaseDeAlguma = set() # Conjunto de conjuntos das pedras que servem de base pra outra.
-
-for conjuntoPedra in listaPedras:
-	# Para cada pedra, ver se ela depende só de uma:
+estacionarTijolos(listaTijolos, coordenadasOcupadas)
+# Agora que todos caíram, ver para cada tijolo, quais ele está em cima.
+# Se estiver em cima de apenas um, é porque esse é a unica base para ele.
+tijolosBaseDeAlgum = set() # Conjunto de conjuntos das pedras que servem de base pra outra.
+for tijolo in listaTijolos:
 	conjuntoCaidoUmaUnidade = set()
-	for coordenada in conjuntoPedra:
+	for coordenada in tijolo:
 		z = coordenada[2]
 		coordenadaInferior = (coordenada[0], coordenada[1], z-1)
 		conjuntoCaidoUmaUnidade.add(coordenadaInferior)
 	# Agora tenho o conjunto das coordenadas inferiores. Ver a interseção com cada uma das pedras.
-	listaPedrasDeBase = []
-	for conjuntoPedra2 in listaPedras:
-		if conjuntoPedra == conjuntoPedra2:
+	listaTijolosDeBase = []
+	for tijolo2 in listaTijolos:
+		if tijolo == tijolo2:
 			continue
-		#print('pedra1:', conjuntoPedra)
-		#print('Embaixo dela:', conjuntoCaidoUmaUnidade)
-		#print('Pedra2:', conjuntoPedra2)
-		#print('Intersecao:', conjuntoPedra2.intersection(conjuntoCaidoUmaUnidade))
-		#print('---------------------')
-		if len(conjuntoPedra2.intersection(conjuntoCaidoUmaUnidade)):
-			#print('appendou!')
-			listaPedrasDeBase.append(conjuntoPedra2)
-		#input()
-	if len(listaPedrasDeBase) == 1: # Só está apoiado em um. Se esse sumir, cai.
-		pedrasBaseDeAlguma.add(frozenset(listaPedrasDeBase[0]))
-
-# Eu tenho:
-# listaPedras -> Lista com todas as pedras estacionadas [{(x,y), (a,b)} , {...}]
-
-print(len(listaPedras) - len(pedrasBaseDeAlguma))
+		if len(tijolo2.intersection(conjuntoCaidoUmaUnidade)):
+			listaTijolosDeBase.append(tijolo2)
+	if len(listaTijolosDeBase) == 1: # Só está apoiado em um. 
+		tijolosBaseDeAlgum.add(frozenset(listaTijolosDeBase[0]))
+print('A quantidade de tijolos desintegráveis é:', len(listaTijolos) - len(tijolosBaseDeAlgum))
 
 #parte 2:
-
-print('bases:', len(pedrasBaseDeAlguma))
 resposta = 0
-for indiceBase, base in enumerate(pedrasBaseDeAlguma): # Remover essa e ver quantas caem.
-	listaSemBase = [pedra for pedra in copy.deepcopy(listaPedras) if pedra != base]
-	coordenadasOcupadasSemBase = copy.deepcopy(coordenadasOcupadas)
+for indiceBase, base in enumerate(tijolosBaseDeAlgum):
+	listaSemABase = [pedra for pedra in copy.deepcopy(listaTijolos) if pedra != base]
+	coordenadasOcupadasSemABase = copy.deepcopy(coordenadasOcupadas)
 	for coordenada in base:
-		coordenadasOcupadasSemBase.remove(coordenada)
-	resposta+= estacionarPedras(listaSemBase, coordenadasOcupadasSemBase)
-	if indiceBase % 50 == 0:
-		print('Feita base:', indiceBase)
-print(resposta)
+		coordenadasOcupadasSemABase.remove(coordenada)
+	resposta+= estacionarTijolos(listaSemABase, coordenadasOcupadasSemABase)
+print('A soma da quantidade de reações em cadeia é de:', resposta)
